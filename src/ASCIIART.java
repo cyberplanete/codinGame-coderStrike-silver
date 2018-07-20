2b3f6e405dc7f4a0fb29a558e341cd86420bab46
pascal <pascal@pascal-desktop>
2018-07-20 18:34:34 +0200

    .

--- src/ASCIIART.java
+++ src/ASCIIART.java
@@ -2,122 +2,191 @@ import java.util.*;
 import java.io.*;
 import java.math.*;
 
+/**
+ * Auto-generated code below aims at helping you parse the standard input
+ * according to the problem statement.
+ **/
+class Solution {
 
-public class ASCIIART {
-private static List<String> listMots = new ArrayList<String>();
-    
-    public static void main(String args[]) {
-        Scanner in = new Scanner(System.in);
-        int L = in.nextInt();
-        int H = in.nextInt();
-        if (in.hasNextLine()) {
-            in.nextLine();
-        }
-        
-        String T = in.nextLine();
-        
-        
-        if (L <= 4) {
-            
-             for (int i = 0; i < H; i++) {
-            String ROW = in.nextLine();
-            System.err.println("ROW :" + ROW);
-                
-                String unMot = T.toUpperCase();        
-            
-                String str = ROW;
-                String [] arrOfStr = str.split("(?<=\\G.{4})");
-                
-                if (unMot.equalsIgnoreCase("E") ) {
-                   listMots.add(arrOfStr[4]);
-                   System.err.println("ici :" + arrOfStr[4]);
-                   
-                    }
-                
-                 if (unMot.equalsIgnoreCase("MANHATTAN") ) {
-                   listMots.add(arrOfStr[12] +arrOfStr[0] + arrOfStr[13] + arrOfStr[7]
-                        + arrOfStr[0] + arrOfStr[19] + arrOfStr[19] + arrOfStr[0] + arrOfStr[13] );//M
-                   
-                   
-                   
-                   System.err.println("ici :" + arrOfStr[4]);
-                   
-                    }
-                
-                if (unMot.equalsIgnoreCase("M@NH@TT@N") ) {
-                   listMots.add(arrOfStr[12] +arrOfStr[26] + arrOfStr[13] + arrOfStr[7]
-                        + arrOfStr[26] + arrOfStr[19] + arrOfStr[19] + arrOfStr[26] + arrOfStr[13] );//M
-                   
-                   
-                   
-                   System.err.println("ici :" + arrOfStr[4]);
-                   
-                    }
-            
-                }
-        
-            }
-            else {
-                
-                 for (int i = 0; i < H; i++) {
-            String ROW = in.nextLine();
-            System.err.println("ROW :" + ROW);
-                
-                String unMot = T.toUpperCase();        
-            
-                String str = ROW;
-                String [] arrOfStr = str.split("(?<=\\G.{20})");
-                
-                if (unMot.equalsIgnoreCase("E") ) {
-                   listMots.add(arrOfStr[4]);
-                   System.err.println("ici :" + arrOfStr[4]);
-                   
-                    }
-                
-                 if (unMot.equalsIgnoreCase("MANHATTAN") ) {
-                   listMots.add(arrOfStr[12] +arrOfStr[0] + arrOfStr[13] + arrOfStr[7]
-                        + arrOfStr[0] + arrOfStr[19] + arrOfStr[19] + arrOfStr[0] + arrOfStr[13] );//M
-                   
-                   
-                   
-                   System.err.println("ici :" + arrOfStr[4]);
-                   
-                    }
-                
-                if (unMot.equalsIgnoreCase("M@NH@TT@N") ) {
-                   listMots.add(arrOfStr[12] +arrOfStr[26] + arrOfStr[13] + arrOfStr[7]
-                        + arrOfStr[26] + arrOfStr[19] + arrOfStr[19] + arrOfStr[26] + arrOfStr[13] );//M
-                   
-                   
-                   
-                   System.err.println("ici :" + arrOfStr[4]);
-                   
-                    }
-            
-                }
-                
-                
-                
-                }
-        
-       
-        
-        for (String unMot : listMots) {
-                    
-                    System.out.println(unMot);
-        
-                    
-                    }
-                
+	// private static List<String> listMots2 = new ArrayList<String>();
+	public static void main(String args[]) {
 
-        // Write an action using System.out.println()
-        // To debug: System.err.println("Debug messages...");
-System.err.println("**********************************************************");
-System.err.println("L :" + L);
-System.err.println("H :" + H);
-System.err.println("T " + T);
+		List<String> listMots = new ArrayList<String>();
+		String[] listMots1 = { "", "", "", "", "", "" };
+		String[] listMots2 = new String[20];
+		String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ?";
+		String[] arrOfAlphabet = alphabet.split("(?<=\\G.{1})");
 
- 
+		String[] arrOfStr;
+		StringBuilder sb = new StringBuilder();
+		Scanner in = new Scanner(System.in);
+		int L = in.nextInt();
+		int H = in.nextInt();
+		if (in.hasNextLine()) {
+			in.nextLine();
+		}
 
-    }
+		String T = in.nextLine();
+
+		if (L == 4) {
+
+			for (int i = 0; i < H; i++) {
+				String ROW = in.nextLine();
+				System.err.println("Mon ROW :" + ROW);
+				arrOfStr = ROW.split("(?<=\\G.{4})");
+				sb.setLength(0);
+
+				if (T.length() > 1) {
+					String unMot = T.toUpperCase();
+					String[] arrOfT = unMot.split("(?<=\\G.{1})");
+
+					List<String> monAlphabet = new ArrayList<String>(Arrays.asList(arrOfAlphabet));
+
+					for (int g = 0; g < arrOfT.length; g++) {
+
+						if (!monAlphabet.contains(arrOfT[g])) {
+
+							sb.append(arrOfStr[26]);
+							String unString = sb.toString();
+							System.err.println("lettre inconnu:" + g);
+							listMots1[i] = unString;
+
+						} else {
+
+							for (int f = 0; f < arrOfAlphabet.length; f++) {
+								String uneLettre = arrOfAlphabet[f];
+								/* System.err.println("Alphabet:" + arrOfAlphabet[f]); */
+								// Index de la lettre si trouvé
+								if (uneLettre.equalsIgnoreCase(arrOfT[g])) {
+									/*
+									 * System.err.println("lettre :" + arrOfT[g]); System.err.println("lettre :" +
+									 * arrOfStr[f]);
+									 */
+									sb.append(arrOfStr[f]);
+									String unString = sb.toString();
+									System.err.println("lettre connue:" + g);
+									listMots1[i] = unString;
+								}
+
+							}
+
+						}
+
+					}
+
+				}
+
+				else {
+					System.err.println("ici:" + i);
+					for (int f = 0; f < arrOfAlphabet.length; f++) {
+						String uneLettre = arrOfAlphabet[f];
+						/* System.err.println("Alphabet:" + arrOfAlphabet[f]); */
+						// Index de la lettre si trouvé
+						if (uneLettre.equalsIgnoreCase(T)) {
+							// System.err.println("lettre :" + arrOfAlphabet[f]);
+							// System.err.println("lettre :" + arrOfStr[f]);
+							listMots1[i] = arrOfStr[f];
+						}
+
+					}
+
+				}
+
+			}
+
+			for (String unMot : listMots1) {
+
+				System.out.println(unMot);
+				// System.err.println(listMots.size());
+
+			}
+
+		} else {
+			/*
+			 * System.err.println("H :" + H); System.err.println("L :" + L);
+			 */
+
+			for (int v = 0; v < 11; v++) {
+				String ROW = in.nextLine();
+				System.err.println(ROW.length());
+				arrOfStr = ROW.split("(?<=\\G.{20})");
+				sb.setLength(0);
+				String unString = "";
+				if (T.length() > 1) {
+					/* String unMot = T.toUpperCase(); */
+					String unMot = T.toUpperCase();
+					String[] arrOfT = unMot.split("(?<=\\G.{1})");
+
+					List<String> monAlphabet = new ArrayList<String>(Arrays.asList(arrOfAlphabet));
+
+					for (int g = 0; g < arrOfT.length; g++) {
+						String maLettreT = arrOfT[g];
+
+						if (!monAlphabet.contains(arrOfT[g])) {
+
+							sb.append(arrOfStr[26]);
+							unString = sb.toString();
+
+							listMots2[v] = sb.toString();
+
+						} else {
+
+							for (int f = 0; f < arrOfAlphabet.length; f++) {
+								String uneLettre = arrOfAlphabet[f];
+								// System.err.println("Alphabet:" + arrOfAlphabet[f]);
+								// Index de la lettre si trouvé
+
+								if (uneLettre.equalsIgnoreCase(maLettreT)) {
+									/*
+									 * System.err.println("lettre :" + arrOfT[g]); System.err.println("lettre :" +
+									 * arrOfStr[f])
+									 */;
+
+									sb.append(arrOfStr[f]);
+									unString = unString + arrOfStr[f];
+
+									listMots2[v] = unString;
+
+								}
+
+							}
+
+						}
+
+					}
+
+				}
+
+				else {
+					System.err.println("ici:" + v);
+					for (int f = 0; f < arrOfAlphabet.length; f++) {
+						String uneLettre = arrOfAlphabet[f];
+						/* System.err.println("Alphabet:" + arrOfAlphabet[f]); */
+						// Index de la lettre si trouvé
+						if (uneLettre.equalsIgnoreCase(T)) {
+							System.err.println("lettre :" + arrOfAlphabet[f]);
+							// System.err.println("lettre :" + arrOfStr[f]);
+
+							listMots2[v] = arrOfStr[f];
+						}
+
+					}
+
+				}
+
+			}
+
+			for (String unMot : listMots2) {
+
+				if (unMot != null) {
+					System.out.println(unMot);
+				}
+
+				// System.err.println(listMots.size());
+
+			}
+
+		}
+
+	}
 }
